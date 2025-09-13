1e99693626eb1ec244217e6e7b1436de29c781b6
Sam Bossley <sam@bossley.xyz>
2025-09-13 12:14:23 -0400

    feat: add module docs

--- src/lib.d.ts
+++ src/lib.d.ts
@@ -1,1 +1,7 @@
+/**
+ * Given a RMD input string, parse and return the corresponding HTML
+ * string. Errors can be thrown.
+ * @param {string} input
+ * @return {Promise<string>} output
+ */
 export function parseRMD(input: string): Promise<string>;
