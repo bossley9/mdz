9395d9ca58a48270c7a46543f2a2ec67ee04bc08
Sam Bossley <sam@bossley.xyz>
2025-09-15 08:45:36 -0400

    feat: autogenerate documentation

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 An smart subset of CommonMark with added popular extensions implemented in Zig for CLI and WASM
 
-For documentation, motivation, and examples, see the [specification](./doc/specification.md).
+For documentation, motivation, and examples, see the generated [specification](./dist/specification.md).
---
+++ build.ts
@@ -0,0 +1,42 @@
+// binary generation
+
+await new Deno.Command("zig", { args: ["build", "wasm"] }).output();
+
+await Deno.mkdir("./dist").catch(() => {}); // ignore if already exists
+
+await Deno.copyFile("./src/lib.d.ts", "./dist/index.d.ts");
+
+const marker = "/*generated_code_flag_marker*/";
+
+const src = await Deno.readTextFile("./src/lib.js");
+const wasm = await Deno.readFile("./zig-out/bin/rmd.wasm");
+
+const output = src.substring(0, src.indexOf(marker)) +
+  wasm +
+  src.substring(src.indexOf(marker) + marker.length);
+
+await Deno.writeTextFile("./dist/index.js", output);
+
+// documentation generation
+
+const doc = await Deno.readTextFile("./src/rmd/specification.zig");
+
+const generatedDoc = doc
+  .split("\n")
+  .map((line) => {
+    if (
+      line.includes("th.expectParseRMD(") ||
+      line.includes("const th = @import") ||
+      line.startsWith("// TODO")
+    ) {
+      return null;
+    } else if (line.startsWith("//")) {
+      return line.substring(line.startsWith("// ") ? 3 : 2);
+    } else {
+      return line;
+    }
+  })
+  .filter((line) => line !== null)
+  .join("\n");
+
+await Deno.writeTextFile("./dist/specification.md", generatedDoc);
